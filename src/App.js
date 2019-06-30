52f0da615d6652de3c4672aeb294c15e504a2989
CiubancanCristian <cristian@ciubancan.com>
2019-06-30 04:28:24 +0300

    added multiple face detection

--- src/App.js
+++ src/App.js
@@ -27,24 +27,25 @@ const particlesOptions = {
   }
 };
 
+const initialState = {
+  input: "",
+  imageUrl: "",
+  boxes: [],
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+};
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-      isClicked: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: ""
-      }
-    };
+  constructor(props) {
+    super(props);
+    this.state = initialState;
   }
 
   loadUser = data => {
@@ -63,22 +64,24 @@ class App extends Component {
     this.setState({ route: route });
   };
 
-  calculateFaceLocation = data => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFacesLocations = data => {
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height
-    };
+
+    return data.outputs[0].data.regions.map(face => {
+      let clarifaiFace = face.region_info.bounding_box;
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height
+      };
+    });
   };
 
-  displayFaceBox = box => {
-    this.setState({ box: box });
+  displayFaceBoxes = boxes => {
+    this.setState({ boxes: boxes });
   };
 
   onInputChange = event => {
@@ -90,13 +93,13 @@ class App extends Component {
     app.models
       .predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
       .then(response =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBoxes(this.calculateFacesLocations(response))
       )
       .catch(err => console.log(err));
   };
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
@@ -118,7 +121,7 @@ class App extends Component {
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
             </div>
           </div>
         ) : route === "signin" ? (
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl, box }) => {
-  return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-      </div>
-    </div>
-  );
-}
+const FaceRecognition = ({ imageUrl, boxes }) => {
+	// const cutii = boxes.outputs[0].data.regions;
+	const faceBoxes = boxes.map((box, index) => {
+		return (
+			<div
+				key={index}
+				className="bounding-box"
+				style={{
+					top: box.topRow,
+					right: box.rightCol,
+					bottom: box.bottomRow,
+					left: box.leftCol
+				}}
+			></div>
+			
+		);
+	});
+
+	return (
+		<div className="center ma">
+			<div className="absolute mt2">
+				<img
+					id="inputimage"
+					alt=""
+					src={imageUrl}
+					width="500px"
+					height="auto"
+				/>
+				{faceBoxes}
+			</div>
+		</div>
+	);
+};
 
 export default FaceRecognition;
